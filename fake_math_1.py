0509f27b9dff57ee32343f175592862bea113d97
SG64ru <sergg64@gmail.com>
2024-10-04 22:41:07 +0500

    Add files via upload

---
+++ fake_math_1.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+    first = int(input("Введите первое значение "))
+    second = int(input("Введите второе значение "))
+    if second == 0:
+        print("Ошибка")
+    else:
+        print(first/second)
+
+
+divide( 1, 1)
+
---
+++ true_math_1.py
@@ -0,0 +1,10 @@
+import math
+def divide(first, second):
+    if second == 0:
+        from math import inf
+        print(math.inf)
+    else:
+        print(first/second)
+
+
+divide( 1, 0)
